d6d76e97b2a6dcf76f6554dc7d8e3607260d1f2c
cavelandiah <cristian@bioinf.uni-leipzig.de>
2021-02-01 23:48:40 +0100

    manual to fill

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,15 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'miRNAture'
-copyright = '2020, Cristian A. Velandia Huerto'
+copyright = '2021, Cristian A. Velandia Huerto'
 author = 'Cristian A. Velandia Huerto'
 
 # The full version, including alpha/beta/rc tags
@@ -31,7 +31,12 @@ release = 'Alpha'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
 ]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'press'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
