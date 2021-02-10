abc2209e7c17b64a253193f495d04bad4b8e4d5f
cavelandiah <cristian@bioinf.uni-leipzig.de>
2021-02-10 14:59:39 +0100

    clean with rfam default

--- Tutorial/Code/run_miRNAture_selected_models.sh
+++ Tutorial/Code/run_miRNAture_selected_models.sh
@@ -7,8 +7,8 @@ specie_name="Latimeria_chalumnae"
 
 workdir="$current/../Results"
 mkdir -p $workdir
-mode="Blast,Final" #,HMM,Infernal,Other_CM,Final"
-strategy="5,ALL"
+mode="Blast,HMM,Infernal,Other_CM,Final"
+strategy="5,6,ALL"
 blastQueriesFolder="$current/../Data/QueriesToTest"
 user_models="$current/User_Test_Data"
 
@@ -22,4 +22,4 @@ user_models="$current/User_Test_Data"
 #miRNAture -stage summarise -speG $specie_genome -speN $specie_name -speT $specie_tag -w $workdir -m $mode -pe 0
 
 # Run miRNAture complete
-miRNAture -stage complete -speG $specie_genome -speN $specie_name -speT $specie_tag -w $workdir -m $mode -pe 0 -str $strategy -blastq $blastQueriesFolder -rep default,150,100 #-usrM $user_models
+miRNAture -stage complete -sublist $current/list_miRNAs_to_search.txt -speG $specie_genome -speN $specie_name -speT $specie_tag -w $workdir -m $mode -pe 0 -str $strategy -blastq $blastQueriesFolder -rep default,150,100 -usrM $user_models
