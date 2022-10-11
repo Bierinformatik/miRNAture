f7faa7ca5c231adbfd736a34ec4f90b35cd32caa
cavelandiah <cristian@bioinf.uni-leipzig.de>
2022-10-11 17:53:35 +0200

    split path bug resolved

--- Tutorial/Code/tutorial_test_selected_models_v1.1.sh
+++ Tutorial/Code/tutorial_test_selected_models_v1.1.sh
@@ -6,10 +6,10 @@ specie_genome="$current/../Data/latimeria_chalumnae_genome.fa"
 specie_name="Latimeria_chalumnae"
 
 workdir="$current/../Results"
-mode="blast,rfam,mirbase,hmm,user,final"
+mode="blast,rfam,mirbase,hmm,final"
 strategy="5,6,ALL"
 blast_queries_folder="$current/../Data/QueriesToTest"
-user_models="$current/User_Test_Data"
+#user_models="$current/User_Test_Data"
 data_precalculated_folder="$current/Dataset_mirnature_tutorial"
 sublistcms="${current}/list_miRNAs_to_search.txt"
 
@@ -22,4 +22,4 @@ mirnature_program="script/miRNAture"
 $mirnature_program -stage complete -mirfix_path ${mirfixF} -sublist ${sublistcms} -nbitscore_cut 0.32 \
                     -dataF ${data_precalculated_folder} -speG ${specie_genome} -speN ${specie_name} \
                     -speT ${specie_tag} -w ${workdir} -m ${mode} -pe 1 -str ${strategy} -blastq ${blast_queries_folder} \
-                    -rep relax,150,100 -usrM ${user_models}
+                    -rep relax,150,100
