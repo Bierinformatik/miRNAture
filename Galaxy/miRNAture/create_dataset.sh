3a927093e1dc8c7459f60332b3c2005fbad150a1
cavelandiah <cavelandiah@unal.edu.co>
2022-11-17 22:52:13 +0100

    improved create data folder

--- Galaxy/miRNAture/create_dataset.sh
+++ Galaxy/miRNAture/create_dataset.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 input=$1
+
 dir=$2
 mkdir $dir/uncompress
 cp $input $dir/uncompress/data.gz
+Dir_name=`tar -tf $dir/uncompress/data.gz | head -1 | cut -f1 -d"/" | sort | uniq`
 tar -xf $dir/uncompress/data.gz --directory $dir/uncompress/
-Dir_name=$(tar -tzf $dir/uncompress/data.gz | head -1 | cut -f1 -d"/" | sort | uniq)
 mv $dir/uncompress/${Dir_name} $dir/uncompress/Dataset
