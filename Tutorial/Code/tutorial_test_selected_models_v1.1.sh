9ab34e1c7c815289deb9d027e58d9ab3b0b0cae8
Cristian Velandia <cavelandiah@tbi.univie.ac.at>
2026-02-27 15:05:02 +0100

    updated yaml to prevent deprecation of clustalw

--- Tutorial/Code/tutorial_test_selected_models_v1.1.sh
+++ Tutorial/Code/tutorial_test_selected_models_v1.1.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 current=$( pwd )
-species_tag="Lach"
+species_tag="Latimeria_chalumnae"
 species_genome="$current/../Data/latimeria_chalumnae_genome.fa"
 species_name="Latimeria_chalumnae"
 
 workdir="$current/../Results"
 #mode="blast,rfam,mirbase,hmm,final"
-mode="user,final"
+mode="rfam,final"
+#mode="user,final"
 strategy="5,6,ALL"
 blast_queries_folder="$current/../Data/QueriesToTest"
 user_models="$current/User_Test_Data"
@@ -15,7 +16,7 @@ data_precalculated_folder="$current/Dataset_mirnature_tutorial"
 sublistcms="${current}/list_miRNAs_to_search.txt"
 
 # Run miRNAture complete
-miRNAture -stage complete -sublist ${sublistcms} -nbitscore_cut 0.32 \
+miRNAture -stage summarise -sublist ${sublistcms} -nbitscore_cut 0.32 \
           -dataF ${data_precalculated_folder} -speG ${species_genome} -speN ${species_name} \
           -speT ${species_tag} -w ${workdir} -m ${mode} -pe 1 -str ${strategy} -blastq ${blast_queries_folder} \
           -rep relax,150,100 -usrM $user_models
